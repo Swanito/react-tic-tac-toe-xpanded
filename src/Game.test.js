6d4205ecba950d611f3515af988d8815d1585a44
González Lavín, Jon <jon.gonzalez.lavin@mobgen.com>
2020-11-10 16:37:49 +0100

    screenshot tests

--- src/Game.test.js
+++ src/Game.test.js
@@ -1,8 +1,32 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import renderer from 'react-test-renderer';
+import Board from './components/Board';
+import Title from './components/Title';
+import Settings from './components/Settings';
+import Square from './components/Square';
+import Game from './Game';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Game Component', () => {
+  const GameComponent = renderer.create(<Game />).toJSON();
+  expect(GameComponent).toMatchSnapshot();
+});
+
+test('renders Title Component', () => {
+  const GameComponent = renderer.create(<Title />).toJSON();
+  expect(GameComponent).toMatchSnapshot();
+});
+
+test('renders Board Component', () => {
+  const GameComponent = renderer.create(<Board />).toJSON();
+  expect(GameComponent).toMatchSnapshot();
+});
+
+test('renders Settings Component', () => {
+  const GameComponent = renderer.create(<Settings />).toJSON();
+  expect(GameComponent).toMatchSnapshot();
+});
+
+test('renders Square Component', () => {
+  const GameComponent = renderer.create(<Square />).toJSON();
+  expect(GameComponent).toMatchSnapshot();
 });
--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -3,6 +3,8 @@ import React from 'react';
 export const GameContext = React.createContext({
     players: 2,
     boardSize: 1,
+    winner: null,
+    setWinner: () => {},
     setPlayer: () => {},
     setBoardSize: () => {},
 });
